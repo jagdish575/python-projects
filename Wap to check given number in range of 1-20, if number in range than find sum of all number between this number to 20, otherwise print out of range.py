fb97921fb332cafc2ca3260b3f99057df2eb8ba7
Jagdish Prajapati <151916824+jagdish575@users.noreply.github.com>
2024-05-22 14:09:14 +0530

    add new program

---
+++ Wap to check given number in range of 1-20, if number in range than find sum of all number between this number to 20, otherwise print out of range.py
@@ -0,0 +1,8 @@
+number=int(input("enter a number :"))
+sum=0
+for i in range(number):
+    sum=sum+i
+if sum==0:
+    print("the given number is not prime")
+else:
+    print("number is out of index")
