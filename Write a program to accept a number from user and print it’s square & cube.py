910a76345eb168ed7944b9d2ea0a393b4e70e4f7
Jagdish Prajapati <151916824+jagdish575@users.noreply.github.com>
2024-05-22 12:06:25 +0530

    new program addd

---
+++ Write a program to accept a number from user and print it’s square & cube.py
@@ -0,0 +1,5 @@
+number=int(input("enter number you want to print his square and qube :"))
+Qube=number*number*number
+square=number*number
+print(f"square of {number} is {square}")
+print(f"Qube of {number} is {Qube}")
